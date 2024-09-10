1d7be6ebd35c18cab22c0587d9e356701b039738
Daniela <danisegd@hotmail.com>
2024-09-10 13:23:53 -0300

    Implements native java double linked list and its methods

--- double-linked-list/src/Main.java
+++ double-linked-list/src/Main.java
@@ -1,5 +1,47 @@
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        LinkedList<Integer> doubleList = new LinkedList<>();
+
+        //Adding
+
+        //Add an element at the end of the list
+        doubleList.add(1);
+        doubleList.add(3);
+        //Add an element at the beginning of the list
+        doubleList.addFirst(2);
+
+
+        //Get
+
+        //Get an element in a specific position
+        System.out.println(doubleList.get(1));
+        //Get an element at the end of the list
+        System.out.println(doubleList.getLast());
+        //Get an element at the beginning of the list
+        System.out.println(doubleList.getFirst());
+
+
+        //Set
+
+        //Change an element in a specific position
+        doubleList.set(0,2);
+
+
+        //Remove
+
+        //Remove an element in a specific position
+        doubleList.remove(0);
+        //Remove an element at the end of the list
+        doubleList.removeLast();
+        //Remove an element at the beginning of the list
+        doubleList.removeFirst();
+
+
+
+
+
     }
 }
