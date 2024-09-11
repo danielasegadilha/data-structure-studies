bd821f1b3cd7606533337fb9b004019891f861b0
Daniela <danisegd@hotmail.com>
2024-09-11 15:41:42 -0300

    Implements on SinglyLinkedList add and remove methods at the beginning

---
+++ linked-list/src/com/daniela/SinglyLinkedList.java
@@ -0,0 +1,47 @@
+package com.daniela;
+
+public class SinglyLinkedList {
+    Node head;
+
+    // Method to add a new node at the beginning
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head != null) {
+            newNode.next = head;
+        }
+        head = newNode;
+    }
+
+    // Method to add a new node at the end
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    // Method to print the linked list
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.print("null");
+    }
+
+    // Method to remove the first element
+    public void removeFirst() {
+        if(head == null) {
+            System.out.println("The list is empty");
+        } else {
+            head = head.next;
+        }
+    }
+}
