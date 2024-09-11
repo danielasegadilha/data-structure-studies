a3f0f4e9bbe12bcd2996470a3d1197a7b91939c9
Daniela <danisegd@hotmail.com>
2024-09-11 17:44:46 -0300

    Implement native Deque interface methods to create a stack

--- queue/src/Main.java
+++ queue/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Deque<String> queue = new ArrayDeque<>();
+
+        //Add an element at the top of the stack
+        queue.addLast("d");
+        queue.addLast("a");
+        queue.addLast("n");
+        queue.addLast("i");
+
+
+        //Get an element at the top of the stack
+        System.out.println(queue.peekFirst());
+
+        //Remove an element at the top of the stack
+        queue.removeFirst();
+
+        //Remove all the element of the stack
+        queue.clear();
     }
 }
--- queue/src/com/daniela/Queue.java
+++ queue/src/com/daniela/Queue.java
@@ -1,4 +1,61 @@
 package com.daniela;
 
 public class Queue {
+    Node front;
+
+    // Method to add a new node at the end of the queue
+    public void push(int data) {
+        Node newNode = new Node(data);
+        if (front == null) {
+            front = newNode;
+        } else {
+            Node current = front;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    // Method to print the stack
+    public void printQueue() {
+        Node current = front;
+        while (current != null) {
+            System.out.print(current.data + " <- ");
+            current = current.next;
+        }
+        System.out.print("null");
+    }
+
+    // Method to remove the first element
+    public void pop() {
+        if(front != null) {
+            front = front.next;
+        }
+    }
+
+    // Method to remove the all elements
+    public void popAll() {
+        front = null;
+    }
+
+    // Method to get the n element, at i index
+    public int get(int i) {
+        Node current = front;
+        while (i != 0) {
+            current = current.next;
+            i --;
+        }
+        return current.data;
+    }
+
+    // Method to update the n element, at "i" index
+    public void set(int newData, int i) {
+        Node current = front;
+        while (i != 0) {
+            current = current.next;
+            i --;
+        }
+        current.data = newData;
+    }
 }
