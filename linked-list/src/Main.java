b7b708e0d390b4e1eca23d63094a768192d5783c
Daniela <danisegd@hotmail.com>
2024-09-11 15:41:36 -0300

    Implements on SinglyLinkedList add and remove methods at the beginning

--- linked-list/src/Main.java
+++ linked-list/src/Main.java
@@ -1,11 +1,19 @@
-import com.daniela.SimpleLinkedList;
+import com.daniela.SinglyLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        SimpleLinkedList list = new SimpleLinkedList();
-        list.add(1);
-        list.add(2);
-        list.add(3);
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.add(0);
+
+        list.printList();
+
+        System.out.print(System.lineSeparator());
+
+        list.removeFirst();
         list.printList();
     }
 }
